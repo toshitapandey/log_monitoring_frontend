6cdc7368b49a56250140f5b95a3d24e2facab6df
Toshita Pandey <toshitapandey@Apples-MacBook-Air.local>
2022-07-16 10:03:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Cable from 'actioncable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    newMessage: "",
+    messages: []
+  }
+
+  handleData = data => {
+    switch (data.type) {
+      case 'current_log':
+        this.setState({
+          messages: data.log
+        })
+        break;
+      case 'new_log':
+        this.setState(prevState => ({
+          messages: [...prevState.messages, data.log]
+        }))
+        break;
+      default:
+        break;
+    }
+  }
+
+  componentDidMount(){
+    this.cable = Cable.createConsumer(`ws://localhost:3001/cable`);
+    this.subscription = this.cable.subscriptions.create({
+      channel: "LogMonitorChannel",
+    }, {
+      connected: () => {console.log('WS connected')},
+      disconnected: () => {console.log('WS disconnected')},
+      received: data => {
+        console.log(data);
+        this.handleData(data);
+      }
+    })
+  }
+  
+  componentWillUnmount(){
+    this.cable.subscriptions.remove(this.subscription);
+  }
+
+  changeHandler = event => {
+    this.setState({
+      newMessage: event.target.value
+    })
+  }
+
+  renderMessages = () => (this.state.messages.map((message, index) => <p key={index}>{message}</p>));
+
+  render() {
+    return (
+      <div className="App">
+        
+        <div>
+          {this.renderMessages()}
+        </div>
+      </div>
+      );
+    }
 }
 
 export default App;
